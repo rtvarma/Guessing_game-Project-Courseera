9817830a97d537ee9cfb1d0132a4396d7cb3594f
rtvarma98 <rtvarma98@gmail.com>
2020-05-14 06:35:16 +0530

    commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,11 +4,13 @@ function guessing_game_function {
     if [[ $1 -lt $2 ]]          
 		then
 		echo "The actual number of files are more than guessed , try again !!"
-    elif [[ $1 -gt $2 ]]
+	fi
+    if [[ $1 -gt $2 ]]
 		then
 		echo "The actual number of files are less than guessed , try again !!"
-    else
-		echo ""
+	fi
+    if [[ $1 -eq $2 ]] 
+		then
 		echo "Congragulations! your guess is right "
     fi
 }
